2679eba38d0433d7ec5104e035b9da9615a4b676
jProgr <8537691@gmail.com>
2016-12-14 23:43:22 +0000

    Game logic

---
+++ tic_tac_toe.js
@@ -0,0 +1,101 @@
+var player_symbol = "x";
+var computer_symbol = "o";
+var grid_content = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+var game_end = false;
+
+function set_player_symbol()
+{
+    player_symbol = this.id;
+    computer_symbol = (this.id === "x") ? "o" : "x";
+    set_game();
+    if (computer_symbol === "x") { computer_turn(); }
+}
+
+function set_game()
+{
+    game_end = false;
+    grid_space.forEach((space) => { space.innerHTML = " "; });
+    grid_content = grid_content.map(() => { return " "; });
+}
+
+function player_turn()
+{
+    if (grid_content[(this.id).split("")[1] - 1] === " ")
+    {
+        grid_content[(this.id).split("")[1] - 1] = player_symbol;
+        check_game_end();
+        computer_turn();
+    }
+}
+
+function computer_turn()
+{
+    while (!game_end)
+    {
+        var x = (Math.random() * 9) | 0;
+        if (grid_content[x] === " ") { grid_content[x] = computer_symbol; break; }
+    }
+    check_game_end();
+    paint();
+}
+
+function check_game_end()
+{
+    // Winning combinations
+    // Check rows from top to bottom then colums from left to right and then diagonals
+    for (var i=0; i<9; i+=3)
+    {
+        var elements_in_row = new Set(grid_content.slice(i, i+3));
+        if (!elements_in_row.delete(" ")) 
+        { if (elements_in_row.size === 1) { console.log(grid_content[i] + ": won"); game_end = true; break; } }
+    }
+    
+    if (!game_end)
+    {
+        for (var i=0; i<3; i++)
+        {
+            var elements_in_column = new Set([grid_content[i], grid_content[i + 3], grid_content[i + 6]]);
+            if (!elements_in_column.delete(" ")) 
+            { if (elements_in_column.size === 1) { console.log(grid_content[i] + ": won"); game_end = true; break; } }
+        }
+    }
+     
+    if (!game_end)
+    {
+        var elements_in_diagonal_1 = new Set([grid_content[0], grid_content[4], grid_content[8]]);
+        if (!elements_in_diagonal_1.delete(" ")) 
+        { if (elements_in_diagonal_1.size === 1) { console.log(grid_content[0] + ": won"); game_end = true; } }
+    }
+    
+    if (!game_end)
+    {
+        var elements_in_diagonal_2 = new Set([grid_content[2], grid_content[4], grid_content[6]]);
+        if (!elements_in_diagonal_2.delete(" ")) 
+        { if (elements_in_diagonal_2.size === 1) { console.log(grid_content[2] + ": won"); game_end = true; } }
+    }
+    
+    // If the grid is full
+    if (!game_end)
+    {
+        var n_symbols = 0;
+        grid_content.forEach((symbol) => { n_symbols += (symbol !== " ") ? 1 : 0; });
+        game_end = n_symbols === 9;
+    }
+}
+
+// UI
+
+function paint()
+{ grid_content.forEach((symbol, index) => { grid_space[index].innerHTML = symbol; }); }
+
+// Get option buttons
+var chooser = [];
+chooser.push(document.getElementById("x"));
+chooser.push(document.getElementById("o"));
+chooser.forEach((button) => { button.addEventListener("click", set_player_symbol, false); });
+
+// Get buttons in grid
+var grid_space = [];
+for (var i=1; i<10; i++) { grid_space.push(document.getElementById("g" + i)); }
+grid_space.forEach((space, i) => { space.addEventListener("click", player_turn, false); });
+
