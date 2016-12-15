cfbd01c7c2c4e72c318a20c7be8cdd08bb32c65f
jProgr <8537691@gmail.com>
2016-12-15 00:59:18 +0000

    Design

--- tic_tac_toe.js
+++ tic_tac_toe.js
@@ -1,8 +1,11 @@
+// This code is awful
+
 var player_symbol = "x";
 var computer_symbol = "o";
-var grid_content = [" ", " ", " ", " ", " ", " ", " ", " ", " "];
+var empty_symbol = "-"
+var grid_content = [empty_symbol, empty_symbol, empty_symbol, empty_symbol, empty_symbol, empty_symbol, empty_symbol, empty_symbol, empty_symbol];
 var game_end = false;
-var winner_symbol = " ";
+var winner_symbol = empty_symbol;
 
 var winner_message_container = document.getElementById("winner_message");
 
@@ -17,8 +20,8 @@ function set_player_symbol()
 function set_game()
 {
     game_end = false;
-    grid_space.forEach((space) => { space.innerHTML = " "; });
-    grid_content = grid_content.map(() => { return " "; });
+    grid_space.forEach((space) => { space.innerHTML = empty_symbol; });
+    grid_content = grid_content.map(() => { return empty_symbol; });
     
     // UI
     winner_message_container.innerHTML = "";
@@ -26,7 +29,7 @@ function set_game()
 
 function player_turn()
 {
-    if (grid_content[(this.id).split("")[1] - 1] === " ")
+    if (grid_content[(this.id).split("")[1] - 1] === empty_symbol)
     {
         grid_content[(this.id).split("")[1] - 1] = player_symbol;
         check_game_end();
@@ -39,7 +42,7 @@ function computer_turn()
     while (!game_end)
     {
         var x = (Math.random() * 9) | 0;
-        if (grid_content[x] === " ") { grid_content[x] = computer_symbol; break; }
+        if (grid_content[x] === empty_symbol) { grid_content[x] = computer_symbol; break; }
     }
     check_game_end();
     paint();
@@ -52,7 +55,7 @@ function check_game_end()
     for (var i=0; i<9; i+=3)
     {
         var elements_in_row = new Set(grid_content.slice(i, i+3));
-        if (!elements_in_row.delete(" ")) 
+        if (!elements_in_row.delete(empty_symbol)) 
         { if (elements_in_row.size === 1) { winner_symbol = grid_content[i]; game_end = true; break; } }
     }
     
@@ -61,7 +64,7 @@ function check_game_end()
         for (var i=0; i<3; i++)
         {
             var elements_in_column = new Set([grid_content[i], grid_content[i + 3], grid_content[i + 6]]);
-            if (!elements_in_column.delete(" ")) 
+            if (!elements_in_column.delete(empty_symbol)) 
             { if (elements_in_column.size === 1) { winner_symbol = grid_content[i]; game_end = true; break; } }
         }
     }
@@ -69,14 +72,14 @@ function check_game_end()
     if (!game_end)
     {
         var elements_in_diagonal_1 = new Set([grid_content[0], grid_content[4], grid_content[8]]);
-        if (!elements_in_diagonal_1.delete(" ")) 
+        if (!elements_in_diagonal_1.delete(empty_symbol)) 
         { if (elements_in_diagonal_1.size === 1) { winner_symbol = grid_content[0]; game_end = true; } }
     }
     
     if (!game_end)
     {
         var elements_in_diagonal_2 = new Set([grid_content[2], grid_content[4], grid_content[6]]);
-        if (!elements_in_diagonal_2.delete(" ")) 
+        if (!elements_in_diagonal_2.delete(empty_symbol)) 
         { if (elements_in_diagonal_2.size === 1) { winner_symbol = grid_content[2]; game_end = true; } }
     }
     
@@ -84,7 +87,7 @@ function check_game_end()
     if (!game_end)
     {
         var n_symbols = 0;
-        grid_content.forEach((symbol) => { n_symbols += (symbol !== " ") ? 1 : 0; });
+        grid_content.forEach((symbol) => { n_symbols += (symbol !== empty_symbol) ? 1 : 0; });
         game_end = n_symbols === 9;
     }
     
@@ -97,7 +100,9 @@ function paint()
 { grid_content.forEach((symbol, index) => { grid_space[index].innerHTML = symbol; }); }
 
 function print_winner()
-{ winner_message_container.innerHTML = winner_symbol + " wins!"; }
+{
+    winner_message_container.innerHTML = (winner_symbol === empty_symbol) ? "Draw" : winner_symbol + " wins";
+}
 
 // Get option buttons
 var chooser = [];
